481fd922f0a61bf94f38f0deeef38b035af76a12
hotavio10 <hotavio10@gmail.com>
2024-05-09 21:03:43 -0300

    feat: add init

---
+++ server/src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    console.log('Listagem de usuários');
+
+
+
+    response.json([
+        'Douglas',
+        'Cleiton',
+        'Robson',
+        'daniel',
+        'teste',
+        'pedro'
+     ]);
+});
+
+app.listen(3333)

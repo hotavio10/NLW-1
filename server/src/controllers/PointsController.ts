1e3c54e744bb67a594611c6c66970d144df054e6
hotavio10 <hotavio10@gmail.com>
2024-05-13 22:03:13 -0300

    feat: add List of Points

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -2,7 +2,18 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
-    async create (request: Request, response: Response)  {
+    async show(request: Request, response: Response) {
+        const { id } = request.params;
+
+        const point = await knex('points').where('id', id).first();
+
+        if(!point) {
+            return response.status(400).json({ message: 'Point not found.'});
+        }
+        return response.json(point);
+    }
+
+    async create(request: Request, response: Response) {
         const {
             name,
             email,
@@ -13,10 +24,10 @@ class PointsController {
             uf,
             items
         } = request.body;
-    
+
         const trx = await knex.transaction();
-    
-        const insertedIds = await knex('points').insert({
+
+        const point = {
             image: 'image-fake',
             name,
             email,
@@ -25,20 +36,25 @@ class PointsController {
             longitude,
             city,
             uf
-        });
-    
+        };
+
+        const insertedIds = await trx('points').insert(point);
+
         const point_id = insertedIds[0];
-    
+
         const pointItems = items.map((item_id: number) => {
             return {
                 item_id,
                 point_id,
             };
         })
-    
+
         await knex('point_items').insert(pointItems);
-    
-        return response.json({ success: true });
+
+        return response.json({
+            id: point_id,
+            ...point,
+        });
     };
 }
 

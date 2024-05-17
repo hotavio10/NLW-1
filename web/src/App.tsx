28d3b04a3e3f66b20c97e41db072ae114d7b1525
hotavio10 <hotavio10@gmail.com>
2024-05-16 21:05:26 -0300

    feat: add Header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import Header from './Header';
+
 function App() {
   return (
+   <div>
+    <Header />
 
-    <h1>Hello</h1>
+    <h1>Conteúdo da aplicação</h1>
+   </div>
   );
 }
+
 export default App;
